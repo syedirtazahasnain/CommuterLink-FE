c0c187873888493857211c834513e13b6d1ac09a
Hassan Raza <hassanrazamalik512@gmail.com>
2023-08-15 21:41:51 -0700

    BackendLayout settings

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import { useSelector } from "react-redux";
 import Faq from './pages/frontend/faq/Faq';
 import Login from './pages/frontend/login/Login';
 import SeatCostVerification from './pages/frontend/register-form/SeatCostVerification';
-import Dashboard1 from './pages/backend/dashboard/Dashboard1';
+import BackendLayout from './pages/backend/BackendLayout';
 import CommuterProfile1 from './pages/backend/dashboard/CommuterProfile1';
 import ReplyToConfirmDates1 from './pages/backend/dashboard/ReplyToConfirmDates1';
 import SendApprovalForPartner1 from './pages/backend/dashboard/SendApprovalForPartner1';
@@ -28,6 +28,7 @@ import Verification1 from './pages/backend/dashboard/Verification1';
 import WhyProcessPayment1 from './pages/backend/dashboard/WhyProcessPayment1';
 import ShareToCare1 from './pages/backend/dashboard/ShareToCare1';
 import Office_School from './pages/frontend/register-form/Office_School';
+import MembersDetails from './pages/backend/dashboard/MembersDetails';
 function App() {
   const userLogin = useSelector((s) => s.login.data.token);
   const userSignup = useSelector((s) => s.signup.data.token);
@@ -45,15 +46,17 @@ function App() {
         <Route path='/signup' element={<Signup />} />
         <Route path='/login' element={<Login />} />
         <Route path='/faq' element={<Faq/>}/>
-        <Route path='/commuterprofile1' element={<CommuterProfile1 />}/>
-        <Route path='/replytoconfirmdates1' element={<ReplyToConfirmDates1 />}/>
-        <Route path='/sendapprovalforpartner1' element={<SendApprovalForPartner1 />}/>
-        <Route path='/termscondition1' element={<TermsCondition1 />}/>
-        <Route path='/verification1' element={<Verification1 />}/>
-        <Route path='/whyprocesspayment1' element={<WhyProcessPayment1 />}/>
-        <Route path='/sharetocare1' element={<ShareToCare1 />}/>
+
         {/* <Route path='/office_school' element={<Office_School />}/> */}
-        <Route path='/new-dashboard' element={<Dashboard1 />}/>
+        <Route path='/new-dashboard' element={<BackendLayout><MembersDetails /></BackendLayout>} />
+        <Route path='/commuterprofile1' element={<BackendLayout><CommuterProfile1 /></BackendLayout>} />
+        <Route path='/replytoconfirmdates1' element={<BackendLayout><ReplyToConfirmDates1 /></BackendLayout>} />
+        <Route path='/sendapprovalforpartner1' element={<BackendLayout><SendApprovalForPartner1 /></BackendLayout>} />
+        <Route path='/sharetocare1' element={<BackendLayout><ShareToCare1 /></BackendLayout>} />
+        <Route path='/termscondition1' element={<BackendLayout><TermsCondition1 /></BackendLayout>} />
+        <Route path='/verification1' element={<BackendLayout><Verification1 /></BackendLayout>} />
+        <Route path='/whyprocesspayment1' element={<BackendLayout><WhyProcessPayment1 /></BackendLayout>} />
+
         <Route path='/number-generate' element={<NumberGenerate/>}></Route>
 
           {userOption0 && (
