6d3d632119c14711ff9ded0219d942a95fe9f84e
sadaf1 <sadaf.bsse3715@iiu.edu.pk>
2023-09-20 11:38:34 +0500

    notifications

--- src/helpers.js
+++ src/helpers.js
@@ -7,3 +7,25 @@ export const getAPIURL = (url = '') => {
 export const getBaseURL = (url = '') => {
     return `${BASE_URL}${url}`;
 };
+
+export const displayNotification = (type, text, layout = "bottomRight") => {
+    let animation = {
+        open: 'animate__animated animate__fadeInRight', 
+        close: 'animate__animated animate__fadeOutRight' 
+    };
+    if (layout == "bottomRight") {
+        animation = {
+            open: 'animate__animated animate__slideInRight',
+            close: 'animate__animated animate__slideOutRight'
+        }
+    }
+    const noty = new Noty({
+        type,
+        text,
+        layout,
+        timeout: 5000,
+        animation,
+        closeWith: ['click', 'button'],
+    });
+    noty.show();
+}
