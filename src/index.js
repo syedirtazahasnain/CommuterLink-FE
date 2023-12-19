438a0b7a52122a44f29051274afd37da1490f853
SYED IRTAZA <arsal.h@yahoo.com>
2023-12-19 12:37:37 +0500

    fixes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
-import { Client_Id } from "../../constants";
+import { Client_Id } from "../src/constants";
 import { persistStore } from 'redux-persist';
 import store from "./redux/store";
 import { GoogleOAuthProvider } from '@react-oauth/google';
